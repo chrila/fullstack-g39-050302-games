4c18f588ee4119b34af6fa8cbcf5b01c7195b67e
Christian Lampl <christian.lampl@gmx.at>
2021-02-07 12:26:50 -0300

    Bfix: Game: validations of age and number of players

--- app/models/game.rb
+++ app/models/game.rb
@@ -14,14 +14,14 @@ class Game < ApplicationRecord
 
   def check_age
     return unless age_from.present? && age_to.present?
-    return if age_from < age_to
-    errors.add(:base, 'age (from) must be smaller than age (to)')
+    return if age_from <= age_to
+    errors.add(:base, 'age (from) must be less than or equal to age (to)')
   end
 
   def check_players
     return unless players_from.present? && players_to.present?
-    return if players_from < players_to
-    errors.add(:base, 'players (from) must be smaller than players (to)')
+    return if players_from <= players_to
+    errors.add(:base, 'players (from) must be less than or equal to players (to)')
   end
   
 end
